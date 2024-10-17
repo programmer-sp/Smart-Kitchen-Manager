b765bae15e9253923940f8d9908d1bc8d7f845c5
Arundarasi89 <arundarasi.rajendran@gmail.com>
2024-10-17 10:29:13 -0400

    chore: modified frontend pages

--- backend/src/common/loaders/redis.ts
+++
@@ -1,4 +0,0 @@
-import { createClient } from "redis";
-import config from '../config';
-
-export const redisClient = createClient({ url: `redis://${config.REDIS_URL}:${config.REDIS_PORT}`, password: config.REDIS_PASSWORD });
